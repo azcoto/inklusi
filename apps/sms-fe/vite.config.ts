ef3d2ca131f2793bb99266ba0d56265d64091ae5
Arizal Coto <rizal.zc@gmail.com>
2022-06-27 12:18:13 +0700

    fix for deploy prod

--- apps/sms-fe/vite.config.ts
+++ apps/sms-fe/vite.config.ts
@@ -4,23 +4,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   build: {
     outDir: './dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('@mantine')) {
-            return 'vendor_mantine';
-          } else if (id.includes('tabler-icons-react')) {
-            return 'vendr_tir';
-          }
-          return 'vendor';
-        },
-      },
-    },
   },
   plugins: [
     tsconfigPaths({
